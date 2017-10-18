ec82535cc1f7a2a0565e614d8310918228dc00fc
Christopher Sanchez <chrisan1992@gmail.com>
2017-10-17 21:01:05 -0600

    Change namespace
    Change namespace

--- ALM Upgrade/Models/Customer_ProjectMetaData.cs
+++ ALM Upgrade/Models/Customer_ProjectMetaData.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace NOC_Macro.Models
+namespace ALM_Upgrade.Models
 {
 
     [MetadataType(typeof(Customer_ProjectMetaData))]
