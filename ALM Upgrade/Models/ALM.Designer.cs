01c2bc0da28a3fe3fdc2e08351cd05902d6779d6
Christopher Sanchez <chrisan1992@gmail.com>
2017-10-18 22:29:11 -0600

    Emails

--- ALM Upgrade/Models/ALM.Designer.cs
+++ ALM Upgrade/Models/ALM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Christopher\source\repos\ALM Upgrade\ALM Upgrade\Models\ALM.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Christopher\Documents\GitHub\ALM-Upgrade\ALM Upgrade\Models\ALM.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
